4ff5bdc72499d8fbb07a01ff85d46a0d13bfa1c7
Morgan Wilde <morgan@wilde.work>
2023-02-23 21:00:59 +0200

    Added a check for Node.

--- source/utilities/browser.js
+++ source/utilities/browser.js
@@ -1,14 +1,22 @@
-const userAgent = navigator.userAgent.toLowerCase();
+let browser = {};
 
-const browser = {
-  isSafari: /safari/i.test(userAgent) && !/(chrome|chromium)/i.test(userAgent),
-  isChrome: /chrome/i.test(userAgent),
-};
+if (typeof navigator !== 'undefined') {
+  const userAgent = navigator.userAgent.toLowerCase();
 
-if (browser.isSafari) {
-  browser.name = 'safari';
-} else if (browser.isChrome) {
-  browser.name = 'chrome';
+  browser = {
+    isSafari: /safari/i.test(userAgent) && !/(chrome|chromium)/i.test(userAgent),
+    isChrome: /chrome/i.test(userAgent),
+  };
+  
+  if (browser.isSafari) {
+    browser.name = 'safari';
+  } else if (browser.isChrome) {
+    browser.name = 'chrome';
+  }  
+} else {
+  browser = {
+    isNode: true
+  };
 }
 
 export {browser};
